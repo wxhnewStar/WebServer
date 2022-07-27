2e66e795791784307c034d7635041036e50b0831
wxh <wxh_newStar@foxmail.com>
2022-07-27 23:50:00 +0800

    stash: add some detail of http_conn & readme

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -25,6 +25,64 @@
 class http_conn
 {
 public:
+    static const int FILENAME_LEN = 200;
+    static const int READ_BUFFER_SIZE = 2048;
+    static const int WRITE_BUFFER_SIZE = 1024;
+
+    enum METHOD
+    {
+        GET = 0,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT,
+        PATH
+    };
+
+    enum CHECK_STATE
+    {
+        CHECK_STATE_REQUESTLINE = 0,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
+
+    enum HTTP_CODE
+    {
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+
+    enum LINE_STATUS
+    {
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
+
+
+public:
+    http_conn() {}
+    ~http_conn() {}
+
+public:
+    void init( int sockfd, const sockaddr_in& addr );
+    void close_conn( bool real_close = true );
+    void process();
+    bool read_once();
+    bool write();
+    sockadd* get_address()
+    {
+        
+    }
 
 private:
 
